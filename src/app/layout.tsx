1b13dba154372357b87cd9c38c342460ee69302b
quintas <caioquintassantiago@gmail.com>
2025-04-26 00:05:43 -0300

    fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
     title: "Projeti",
-    description: "Projeti,
+    description: "Projeti",
 };
 
 export default function RootLayout({
